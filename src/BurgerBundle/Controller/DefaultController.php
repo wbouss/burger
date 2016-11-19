a4c81b498a8790fa6e44865d143455996fe260fd
= <wbouss@gmail.com>
2016-11-19 16:19:09 +0100

    Page carte: mise en page des produits

--- src/BurgerBundle/Controller/DefaultController.php
+++ src/BurgerBundle/Controller/DefaultController.php
@@ -8,12 +8,22 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class DefaultController extends Controller
 {
     /**
-     * Matches /blog exactly
+     * Matches 
      *
      * @Route("/burger", name="burger_homepage")
      */
     public function indexAction()
     {
         return $this->render('BurgerBundle:Default:main.html.twig');
+    }
+    
+    /**
+     * Matches 
+     *
+     * @Route("/carte", name="burger_carte")
+     */
+    public function carteAction()
+    {
+        return $this->render('BurgerBundle:Default:carte.html.twig');
     }
 }
